7ff006831958101465bd60040721a464e2405c37
Taleem <taleemmankuer@gmail.com>
2023-07-31 17:13:08 +0300

    add footer container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { useEffect } from "react";
 import styles from "./styles/Home.module.scss";
 import { Header, Projects, Skills, Aboutme, Contact } from "./components";
-import { AiOutlineHeart } from "react-icons/ai";
-import { BsLinkedin } from "react-icons/bs";
-import { FaGithub } from "react-icons/fa";
 import "animate.css";
 import WOW from "wow.js";
+import Footer from "./components/Footer";
 
 function App() {
   useEffect(() => {
@@ -20,28 +18,7 @@ function App() {
         <Skills />
         <Aboutme />
         <Contact />
-        <footer className={styles.footer}>
-          <div className={styles.icons}>
-            <a
-              href="https://linkedin.com/in/taleem-mankuer-713367235"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <BsLinkedin />
-            </a>
-            <a
-              href="https://github.com/Taleemj"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FaGithub />
-            </a>
-          </div>
-          <h4>
-            Made & designed with <AiOutlineHeart /> By <span>Taleem</span>
-            {new Date().getFullYear()}
-          </h4>
-        </footer>
+        <Footer styles={styles} />
       </div>
     </>
   );
